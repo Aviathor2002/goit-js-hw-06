a6c7663c9c6471952b317df26f47cf36801b116e
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2022-11-13 04:07:31 +0200

    task 2

--- task_2.js
+++ task_2.js
@@ -6,3 +6,47 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+const list = document.querySelector("ul");
+
+const ingridientsList = [];
+
+ingredients.forEach((ingredient) => {
+  const createIngr = document.createElement("li");
+  createIngr.textContent = ingredient;
+  createIngr.classList.add("item");
+  ingridientsList.push(createIngr);
+
+  console.log(createIngr);
+});
+
+console.log(ingridientsList);
+list.append(...ingridientsList);
+
+//
+
+// const ingridientsList = ingredients.map((ingredient) => {
+//   const createIngr = document.createElement("li");
+//   createIngr.textContent = ingredient;
+//   createIngr.classList.add("item");
+//   return createIngr;
+// });
+
+// console.log(ingridientsList);
+// list.append(...ingridientsList);
+
+//
+
+// const makeIngridientsList = (elements) => {
+//   return elements.map((element) => {
+//     const createEl = document.createElement("li");
+//     createEl.textContent = element;
+//     createEl.classList.add("item");
+//     return createEl;
+//   });
+// };
+
+// const createIngridientsList = makeIngridientsList(ingredients);
+
+// console.log(createIngridientsList);
+
+// list.append(...createIngridientsList);
