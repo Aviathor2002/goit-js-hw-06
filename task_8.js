7f8e9d4dd82cab6e5a9b6756bf6485f44926f58e
Oleksandr Lytvyniuk <sandro132015@gmail.com>
2022-11-16 11:36:15 +0200

    сhabge method

--- task_8.js
+++ task_8.js
@@ -22,32 +22,6 @@
 
 //
 
-// const form = document.querySelector(".login-form");
-
-// form.addEventListener("submit", onFormSubmit);
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-
-//   const formElements = event.currentTarget.elements;
-//   console.log(formElements);
-
-//   const mail = formElements.email;
-//   const password = formElements.password;
-
-//   if (mail.value === "" || password.value === "") {
-//     return alert("Please fill in all the fields!");
-//   }
-
-//   console.log({
-//     [mail.name]: mail.value,
-//     [password.name]: password.value,
-//   });
-//   event.currentTarget.reset();
-// }
-
-//
-
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", onFormSubmit);
@@ -55,18 +29,43 @@ form.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const formData = new FormData(event.currentTarget);
+  const formElements = event.currentTarget.elements;
 
-  console.log(formData);
+  const mail = formElements.email;
+  const password = formElements.password;
 
-  formData.forEach((value, name) => {
-    if (value === "") {
-      return alert("Please fill in all the fields!");
-    }
-    console.log({ [name]: value });
+  if (mail.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log({
+    [mail.name]: mail.value,
+    [password.name]: password.value,
   });
   event.currentTarget.reset();
 }
+
+//
+
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit", onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.currentTarget);
+
+//   console.log(formData);
+
+//   formData.forEach((value, name) => {
+//     if (value === "") {
+//       return alert("Please fill in all the fields!");
+//     }
+//     console.log({ [name]: value });
+//   });
+//   event.currentTarget.reset();
+// }
 
 // ! так і не розібрався як таким методом через
 // ! formData зробить щоб в консолі було в одному
